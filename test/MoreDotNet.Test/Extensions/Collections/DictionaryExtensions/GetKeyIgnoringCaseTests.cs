cb11d68fe0931d2c2c5d2cd7ba54df6eb4748cda
Tsveta Kamenova <88488070+Tsveta-Kamenova@users.noreply.github.com>
2024-01-02 21:45:22 +0200

    Update GetKeyIgnoringCaseTests.cs

--- test/MoreDotNet.Test/Extensions/Collections/DictionaryExtensions/GetKeyIgnoringCaseTests.cs
+++ test/MoreDotNet.Test/Extensions/Collections/DictionaryExtensions/GetKeyIgnoringCaseTests.cs
@@ -1,5 +1,6 @@
 ﻿namespace MoreDotNet.Test.Extensions.Collections.DictionaryExtensions
 {
+    using System;
     using System.Collections.Generic;
 
     using MoreDotNet.Extensions.Collections;
@@ -45,6 +46,50 @@
 
             Assert.NotNull(actual);
             Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData("TestKey", "testkey")]
+        [InlineData("anotherKey", "ANOTHERKEY")]
+        [InlineData("mIXeDcAseKey", "MiXeDcAsEkEy")]
+        public void GetKeyIgnoringCase_ExistingKeyGiven_ShouldReturnMatchingKey(string originalKey, string caseInsensitiveKey)
+        {
+            this.testDictionary.Add(originalKey, 42);
+
+            string actual = this.testDictionary.GetKeyIgnoringCase(caseInsensitiveKey);
+
+            Assert.Equal(originalKey, actual);
+        }
+
+        [Fact]
+        public void GetKeyIgnoringCase_MultipleKeysWithSameIgnoreCaseGiven_ShouldReturnFirstMatchingKey()
+        {
+            this.testDictionary.Add("TestKey", 1);
+            this.testDictionary.Add("testkey", 2);
+
+            string actual = this.testDictionary.GetKeyIgnoringCase("testkey");
+
+            Assert.Equal("TestKey", actual);
+        }
+
+        [Fact]
+        public void GetKeyIgnoringCase_NullDictionaryGiven_ShouldThrowArgumentNullException()
+        {
+            IDictionary<string, int> nullDictionary = null;
+
+            Assert.Throws<ArgumentNullException>(() => nullDictionary.GetKeyIgnoringCase("someKey"));
+        }
+
+        [Fact]
+        public void GetKeyIgnoringCase_NullCaseInsensitiveKeyGiven_ShouldReturnEmptyString()
+        {
+            Assert.Equal(string.Empty, this.testDictionary.GetKeyIgnoringCase(null));
+        }
+
+        [Fact]
+        public void GetKeyIgnoringCase_EmptyCaseInsensitiveKeyGiven_ShouldReturnEmptyString()
+        {
+            Assert.Equal(string.Empty, this.testDictionary.GetKeyIgnoringCase(""));
         }
     }
 }
